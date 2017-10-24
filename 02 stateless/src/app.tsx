81830fd0b73fdd07ead17b520d0b3bcbf5587b9c
basarat <basaratali@gmail.com>
2017-10-24 16:15:49 +1100

    take it back to the start

--- 02 stateless/src/app.tsx
+++ 02 stateless/src/app.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const App: React.SFC<{ message: string }>
-  = ({ message }) => <div>{message}</div>;
-
 ReactDOM.render(
-  <App message="Hello world!" />,
+  <div>Hello world</div>,
   document.getElementById('root')
 );
